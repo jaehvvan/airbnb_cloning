cccf05ac9876a81c89f1478abd3856cff0a4ca83
Eunjoo <lynn0506@snu.ac.kr>
2020-04-25 20:58:30 +0900

    card-news, online part completed

---
+++ online.js
@@ -0,0 +1,32 @@
+var scrollPos = window.scrollY || document.documentElement.scrollTop
+
+setInterval(function() {
+	console.log(scrollPos);
+	var x = document.getElementById("head");
+	if(scrollPos > 1) {
+		x.style.backgroundColor = 'white';
+		document.getElementById('logo_img').setAttribute('src', 'img/logo.png');	
+		document.getElementById('logo_img').setAttribute('class', 'img_changed');
+		document.getElementById('menus').setAttribute('backgroundColor', 'white');
+		document.getElementById('menu_detail').setAttribute('class', 'menus_changed');
+		document.getElementById('menu_button').setAttribute('class', 'button_changed');
+		document.getElementById('menu_img').setAttribute('src', 'img/globe.png');
+		document.getElementById('menu_img').setAttribute('class', 'img2_changed')
+		for(var i = 0; i<5; i++)
+			document.getElementsByTagName("button")[i].style.color ='black';
+	} else {
+		x.style.backgroundColor = "transparent";
+		document.getElementById('logo_img').setAttribute('src', 'img/logo2.png');
+		document.getElementById('logo_img').setAttribute('class', '');	
+		document.getElementById('menus').setAttribute('backgroundColor', 'black');
+		document.getElementById('menu_detail').setAttribute('class', '');
+		document.getElementById('menu_button').setAttribute('class', '');
+		document.getElementById('menu_img').setAttribute('src', "img/globe2.png");
+		document.getElementById('menu_img').setAttribute('class', '')
+
+		for(var i = 0; i<5; i++)
+			document.getElementsByTagName("button")[i].style.color ='white';		
+	}
+	scrollPos = window.scrollY || document.documentElement.scrollTop;
+
+}, 250);
